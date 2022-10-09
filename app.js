1d5910d4fe69210e241b57ef605772f3eb0f9a58
jahedchowdhury3 <jahedchowdhury3@gmail.com>
2022-10-09 21:07:46 +0600

    shopping cart done

--- app.js
+++ app.js
@@ -12,6 +12,24 @@ function updateProductNumber(product, price, isIncresing) {
     // update total tk
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
+}
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update on the html sub total
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
 }
 // phone plus button event handler
 document.getElementById('phone-plus').addEventListener('click', function () {
